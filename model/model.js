5df3d0f3c85f298ae05397eb25bb7adc3980337c
packer-陈星星 <13246190524@163.com>
2019-08-11 22:01:43 +0800

    Signed-off-by: packer-陈星星 <13246190524@163.com>

--- model/model.js
+++ model/model.js
@@ -197,3 +197,21 @@ export class article extends base{
     this.request({ url: 'index/v1/article/top', type: 'POST', data: param, sCallBack: (res) => { callback(res); } })
   }
 }
+//推广品牌相关
+export class brand extends base {
+  constructor(){
+    super();
+  }
+  //添加
+  add(param, callback) {
+    this.request({ url: 'index/v1/brand/add', type: 'POST', data: param, sCallBack: (res) => { callback(res); } })
+  }
+  //获取
+  getList(param, callback) {
+    this.request({ url: 'index/v1/brand/get', type: 'POST', data: param, sCallBack: (res) => { callback(res); } })
+  }
+  //修改品牌信息
+  update(param, callback) {
+    this.request({ url: 'index/v1/brand/update', type: 'POST', data: param, sCallBack: (res) => { callback(res); } })
+  }
+}
---
+++ pages/my/brand/list/index.js
@@ -0,0 +1,69 @@
+// pages/my/brand/list/index.js
+import {brand} from '../../../../model/model.js';
+let brandModel = new brand();
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    current_page: 0,
+    data: [],
+    last_page: 1
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    this.getBrandList()
+  },
+  /**
+   * 获取品牌列表
+   */
+  getBrandList:function(){
+    let param = { page: this.data.current_page + 1 }
+    if (this.data.current_page == this.data.last_page) {
+      //提示没有数据了
+      return wx.stopPullDownRefresh();
+    }
+    brandModel.getList(param, (res) => {
+      this.data.current_page = res.current_page;
+      this.data.last_page = res.last_page;
+      this.data.data = this.data.data.concat(res.data);
+      this.setData(this.data);
+      wx.stopPullDownRefresh();
+    });
+  },
+  /**
+   * 进入添加品牌页面
+   */
+  goDetail:function(e){
+    let url = '/pages/my/brand/detail/index';
+    if (e.currentTarget.dataset.index!=undefined){
+      let item = this.data.data[e.currentTarget.dataset.index];
+      url = url + '?id=' + item.id + '&brand_name=' + item.brand_name + '&qrcode=' + item.qrcode + '&intro=' + item.intro;
+    }
+    wx.navigateTo({ url: url });
+  },
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
