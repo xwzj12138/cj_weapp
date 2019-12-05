e7fe6b982572056b62187264b0df83147d6ba811
packer-陈星星 <13246190524@163.com>
2019-12-05 15:17:13 +0800

    Signed-off-by: packer-陈星星 <13246190524@163.com>

--- pages/product/detail/index.js
+++ pages/product/detail/index.js
@@ -8,7 +8,10 @@ Page({
    * 页面的初始数据
    */
   data: {
-    id:0,
+    attr_value:null,
+    select_attr_ids:[],
+    buy_info: { buy_num: 1, source: 0, product_id: 0, sku_id: 0, attr_val_path:''},
+    show_product_attr:false,
     price_array:[],
     detail:{}
   },
@@ -17,7 +20,9 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    this.setData(options);
+    this.data.buy_info.product_id = options.id
+    if (options.source) this.data.buy_info.source = options.source
+    this.setData(this.data);
     loginModel.isLogin((res) => {
       this.getDetail();
     });
@@ -27,7 +32,7 @@ Page({
    * 获取商品详情
    */
   getDetail:function(){
-    let param = {id:this.data.id};
+    let param = { id: this.data.buy_info.product_id};
     product.getDetail(param,(res)=>{
       this.setData({ detail: res.data, price_array: res.data.price.split('.')});
     });
@@ -36,11 +41,52 @@ Page({
    * 收藏商品
    */
   collectGoods:function(){
-    let param = { id: this.data.id };
+    let param = { id: this.data.buy_info.product_id };
     product.collect(param, (res) => {
       this.data.detail.is_collect = !this.data.detail.is_collect;
       this.setData(this.data);
     });
+  },
+
+  /**
+   * 显示规格
+   */
+  showProductAttr:function(){
+    this.data.show_product_attr = !this.data.show_product_attr;
+    this.setData(this.data)
+  },
+
+  /**
+   * 选择规格
+   */
+  selectAttr:function(e){
+    this.data.select_attr_ids[e.currentTarget.dataset.index] = e.currentTarget.dataset.id;
+    //组合规格值
+    this.data.buy_info.attr_val_path = this.data.select_attr_ids.join(',');
+    //判断价格问题
+    this.data.detail.sku_group.forEach((e)=>{
+      if (e.attr_val_path == this.data.buy_info.attr_val_path){
+        this.data.buy_info.sku_id = e.id
+        this.data.attr_value = e;
+      }
+    });
+    console.log(this.data.buy_info)
+    this.setData(this.data)
+  },
+
+  /**
+   * 设置购买数量
+   */
+  setBuyNumber:function(e){
+    this.data.buy_info.buy_num = e.detail.value
+    this.setData({ buy_info:this.data.buy_info})
+  },
+
+  /**
+   * 提交订单
+   */
+  submitOrder:function(){
+    console.log(this.data.buy_info)
   },
   /**
    * 页面相关事件处理函数--监听用户下拉动作
@@ -50,16 +96,13 @@ Page({
   },
 
   /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-
+    return { 
+      title: this.data.detail.product_name,
+      path: 'pages/product/detail/index?id=' + this.data.buy_info.product_id +'&source=2',
+      imageUrl: this.data.detail.image
+    }
   }
 })
