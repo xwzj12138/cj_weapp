70d9fdfa565262088be61623a331b8d9205a9360
packer-陈星星 <13246190524@163.com>
2021-06-25 18:02:13 +0800

    Signed-off-by: packer-陈星星 <13246190524@163.com>

--- utils/config.js
+++ utils/config.js
@@ -1,6 +1,6 @@
 export class config{
   constructor(){}
 }
-config.restUrl = 'http://motui.cn/';
+config.restUrl = 'https://mt.xwzj88.cn/';
 config.uploadUrl = config.restUrl+'api/v1/index/upload';
 config.uploadFile = config.restUrl+'uploads';
